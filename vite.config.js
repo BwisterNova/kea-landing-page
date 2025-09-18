25a145a6adb9322a84a0b52f1676bdf49accb58f
BwisterNova <bnovalife@gmail.com>
2025-09-18 14:02:08 +0100

    Update

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/kea-landing-page",
+  base: process.env.DEPLOY_TARGET === "GH" ? "/kea-landing-page" : "/",
 });
